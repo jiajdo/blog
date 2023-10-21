155f580363d5f6b6d8a18038b10eea5a5745a8c8
Jia <jovsofjdo@gmail.com>
2023-10-20 18:42:36 -0700

    Added get route for posts

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import cors from 'cors';
 import express from 'express';
 import 'dotenv/config';
-//import db from '..server/db-connection.js';
+import db from '..server/db-connection.js';
 
 const app = express();
 app.use(cors());
@@ -11,6 +11,21 @@ const PORT = process.env.PORT || 3030;
 app.get('/', (req, res) => {
     res.send('This is the back')
 })
+
+//app.get display posts on the frontend
+
+app.get("/posts", async (req, res) => {
+    try {
+        const {rows: posts} = await db.query("SELECT * FROM posts;");
+        res.send(posts);
+        console.log("posts from server, posts")
+
+    } catch (e) {
+        return res.status(400).json({e})
+    }
+})
+
+//app.post route for making posts
 
 app.listen(PORT, async () => {
     console.log(`Refrigerator running on ${PORT}`)
